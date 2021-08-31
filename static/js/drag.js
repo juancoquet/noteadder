e3fe2126c71caff6151a9e4167f51b4ab489a475
juancoquet <juancoquet@gmail.com>
2021-08-31 23:48:38 +0100

    Replace last used note, remove note if dragged out of bar

--- static/js/drag.js
+++ static/js/drag.js
@@ -6,12 +6,10 @@ window.addEventListener('dragend', setupDraggables);
 function setupDraggables() {
     const draggables = document.querySelectorAll('.draggable')
     draggables.forEach(draggable => {
-        const parent = draggable.parentElement
-        const replacement = draggable.cloneNode(true)
 
         draggable.addEventListener('dragstart', () => {
             draggable.classList.add('dragging')
-        })        
+        })
         draggable.addEventListener('dragend', () => {
             draggable.classList.remove('dragging')
         })
@@ -22,17 +20,34 @@ bar.addEventListener('dragover', e => {
     e.preventDefault()
     const followingElement = getDropPosition(bar, e.clientX)
     const beingDragged = document.querySelector('.dragging')
-    if (followingElement == null) {
-        bar.appendChild(beingDragged)
-    } else {
-        bar.insertBefore(beingDragged, followingElement)
-    }
-    beingDragged.classList.add('placed')
+
+    beingDragged.addEventListener('dragend', () => {
+        beingDragged.classList.remove('dragging')
+        parent = beingDragged.parentElement
+
+        if (followingElement == null) {
+            bar.appendChild(beingDragged)
+        } else {
+            bar.insertBefore(beingDragged, followingElement)
+        }
+
+        if (!beingDragged.classList.contains('placed') && parent.classList.contains('note-group')) {
+            const replacement = beingDragged.cloneNode(true)
+            if (!beingDragged.classList.contains('rest')) {
+                parent.insertBefore(replacement, parent.firstChild)
+            } else {   
+                parent.appendChild(replacement)
+            }
+        }
+
+        beingDragged.classList.add('placed')
+    })
 })
+
 
 function getDropPosition(bar, mouseX) {
     const placedElements = [...bar.querySelectorAll('.placed:not(.dragging)')]
-
+    
     return placedElements.reduce((followingElement, child) => {
         const childBoundaries = child.getBoundingClientRect()
         const offset = mouseX - childBoundaries.left - (childBoundaries.width / 2)
@@ -43,3 +58,37 @@ function getDropPosition(bar, mouseX) {
         }
     }, {offset: Number.NEGATIVE_INFINITY}).element
 }
+
+
+window.addEventListener('dragover', e => {
+    const beingDragged = document.querySelector('.dragging')
+    beingDragged.addEventListener('dragend', () => {
+        const bar = document.querySelector('.bar-container')
+        const barBoundaries = bar.getBoundingClientRect()
+        const x = e.clientX
+        const y = e.clientY
+
+        if (x < barBoundaries.left || x > barBoundaries.right
+            || y < barBoundaries.top || y > barBoundaries.bottom) {
+            if (beingDragged.classList.contains('placed')) {
+                beingDragged.remove()
+            } else {
+                console.log('inside')
+            }
+        }
+    })
+})
+
+// window.addEventListener('mousemove', e => {
+//     const x = e.clientX
+//     const y = e.clientY
+//     const bar = document.querySelector('.bar-container')
+//     const barBoundaries = bar.getBoundingClientRect()
+
+//     if (x < barBoundaries.left || x > barBoundaries.right
+//         || y < barBoundaries.top || y > barBoundaries.bottom) {
+//             console.log('outside')
+//         } else {
+//             console.log('inside')
+//         }
+// })
