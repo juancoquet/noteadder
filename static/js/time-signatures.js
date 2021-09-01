d153f5e940690dc3c5ada58f0db7d91774a9c43b
juancoquet <juancoquet@gmail.com>
2021-09-01 23:03:13 +0100

    Add dotted notes fucntionality

--- static/js/time-signatures.js
+++ static/js/time-signatures.js
@@ -1,6 +1,8 @@
 const timeSigs = document.querySelectorAll('.time-signature');
+const dotToggle = document.querySelector('.dot-toggle');
 
 document.addEventListener('DOMContentLoaded', calculateBlockWidths);
+dotToggle.addEventListener('click', toggleDotted);
 
 timeSigs.forEach(button => {
     button.addEventListener('click', timeSigPress);
@@ -18,6 +20,7 @@ function timeSigPress() {
     const bottomNum = parseFloat(this.getAttribute('bottom-value'));
     const newBarLen = topNum * bottomNum;
     bar.setAttribute('value', newBarLen);   // bar is defined in drag.js
+    bar.setAttribute('absolute-value', newBarLen);
 
     // delete all placed notes
     let placed = document.querySelectorAll('.placed');
@@ -30,11 +33,28 @@ function timeSigPress() {
 }
 
 function calculateBlockWidths() {
-    let noteBlocks = document.querySelectorAll('.note-block');
-    let barValue = bar.getAttribute('value');
+    let noteBlocks = document.querySelectorAll('.note-block:not(.placed)');
+    let barValue = bar.getAttribute('absolute-value');
     noteBlocks.forEach(noteBlock => {
         let noteValue = noteBlock.getAttribute('value');
         let newWidth = 100 / (barValue / noteValue);
         noteBlock.style.width = newWidth + '%';
     })
+}
+
+function toggleDotted() {
+    let noteBlocks = document.querySelectorAll('.note-block:not(.placed)');
+    if (this.checked) {
+        noteBlocks.forEach(block => {
+            let dottedVal = block.getAttribute('value') * 1.5;
+            block.setAttribute('value', dottedVal);
+        })
+    } else {
+        noteBlocks.forEach(block => {
+            let originalVal = block.getAttribute('value') * (2/3);
+            block.setAttribute('value', originalVal);
+        })
+    }
+    calculateBlockWidths();
+    calculateAllowedNotes();
 }
