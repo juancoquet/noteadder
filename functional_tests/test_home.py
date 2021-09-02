7a1184be8634870c559b3f9b59398cd8be648c60
juancoquet <juancoquet@gmail.com>
2021-09-02 12:16:15 +0100

    Set up FT base class with selenium

---
+++ functional_tests/test_home.py
@@ -0,0 +1,25 @@
+from .base import FunctionalTest
+
+
+class NoteDragTest(FunctionalTest):
+
+    def test_can_drag_note_into_bar(self):
+        # A new visitor lands on the hompage and sees several note blocks on the page.
+        qtr_note = self.browser.find_element_by_css_selector('.note-block.quarter-note:not(.rest)')
+        qtr_note_rest = self.browser.find_element_by_css_selector('.note-block.quarter-note.rest')
+        eighth_note = self.browser.find_element_by_css_selector('.note-block.eighth-note:not(.rest)')
+        eighth_note_rest = self.browser.find_element_by_css_selector('.note-block.eighth-note.rest')
+        sixteenth_note = self.browser.find_element_by_css_selector('.note-block.sixteenth-note:not(.rest)')
+        sixteenth_note_rest = self.browser.find_element_by_css_selector('.note-block.sixteenth-note.rest')
+
+        # They also see an empty bar where the can drag the note blocks into.
+        bar = self.browser.find_element_by_css_selector('.bar-container')
+
+        # They try to drag a note into the bar.
+        self.actions.drag_and_drop(qtr_note, bar)
+
+        # When they drop the note, it remains inside the bar.
+        dropped_note = bar.find_element_by_css_selector('*')
+        self.assertEqual(dropped_note, qtr_note)
+
+        # TODO: figure out why drag and drop isn't working
