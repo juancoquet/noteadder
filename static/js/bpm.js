7d7023bdc3730dab2f2740c680cae65d45aeeb81
juancoquet <juancoquet@gmail.com>
2021-10-11 15:08:16 +0100

    Style slider

--- static/js/bpm.js
+++ static/js/bpm.js
@@ -5,6 +5,7 @@ bpmSlider.setAttribute('value', 120);
 
 bpmSlider.addEventListener('input', updateValue);
 bpmSlider.addEventListener('input', updateTempo);
+bpmSlider.addEventListener('input', fillSlider);
 
 
 function updateValue() {
@@ -13,4 +14,14 @@ function updateValue() {
 
 function updateTempo() {
     Tone.Transport.bpm.value = parseInt(bpmSlider.value);
+}
+
+function fillSlider() {
+    let min = parseInt(bpmSlider.getAttribute('min'));
+    let max = parseInt(bpmSlider.getAttribute('max'));
+    let range = max - min;
+    let value = parseInt(bpmSlider.value);
+    let dif = value - min;
+    let percent = dif / range * 100 + '%';
+    bpmSlider.style.background = 'linear-gradient(90deg, var(--col-blush)' + percent + ', var(--col-bg)' + percent + ')';
 }
