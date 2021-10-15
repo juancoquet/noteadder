8fca66de5ff3731d356fcb6699ce4ce979878fd6
juancoquet <juancoquet@gmail.com>
2021-10-15 16:08:35 +0100

    Change admin url

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('na-admin/', admin.site.urls),
     path('', include('note_adder.urls')),
     path('learn/', include('learn.urls')),
     path('contact/', include('contact.urls')),
